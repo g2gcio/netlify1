0156a811beffc9ca3db2675fff08ff01876cafb9
g2gcio <16129831+g2gcio@users.noreply.github.com>
2020-03-10 13:52:23 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,9 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        message_new: 'This is TING first Vue JS on Netlify!'
+        message: 'Hello Meng Seng my buddy!',
+        message_new1: 'This is Lee Yiaw first Vue JS on Netlify!',
+        message_new2: 'You can learn how to do it too ;-)'
     }
 });
 var app2 = new Vue({
